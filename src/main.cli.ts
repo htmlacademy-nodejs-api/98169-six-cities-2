715b4f9f7cb48a49782827f01dfce1bc8a3592ec
Alex <alex.dermenji@space01.co.uk>
2025-07-12 11:33:27 +0100

    remove glob, install json-server

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,29 +1,16 @@
 #!/usr/bin/env node
-/* eslint-disable node/no-unsupported-features/es-syntax */
-import { glob } from 'glob';
 import { CLIApplication } from './cli/cli-application.js';
-import { Command } from './cli/commands/command.interface.js';
-import { resolve } from 'node:path';
+import { HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
 
 async function bootstrap() {
   const cliApplication = new CLIApplication();
-  const commands: Command[] = [];
 
-  const commandFiles = await glob('src/cli/commands/*.command.ts');
+  cliApplication.registerCommands([
+    new ImportCommand(),
+    new HelpCommand(),
+    new VersionCommand(),
+  ]);
 
-  for (const file of commandFiles) {
-    const modulePath = resolve(file);
-
-    const module = await import(modulePath);
-    const CommandClass = Object.values(module).find(
-      (exported) => typeof exported === 'function'
-    ) as new () => Command;
-    if (CommandClass) {
-      commands.push(new CommandClass());
-    }
-  }
-
-  cliApplication.registerCommands(commands);
   cliApplication.processCommand(process.argv);
 }
 
