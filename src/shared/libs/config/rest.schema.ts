f15affd8afb6614c670ac0ca67ef63b0b02742d8
Alex <alex.dermenji@space01.co.uk>
2025-07-19 12:05:29 +0100

    update schema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -1,8 +1,12 @@
 import convict from 'convict';
-import 'convict-format-with-validator';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
 
 export type RestSchema = {
   PORT: number;
+  SALT: string;
+  DB_HOST: string;
 };
 
 export const configRestSchema = convict<RestSchema>({
@@ -11,5 +15,17 @@ export const configRestSchema = convict<RestSchema>({
     format: 'port',
     env: 'PORT',
     default: 4000,
+  },
+  SALT: {
+    doc: 'Salt for password hashing',
+    format: String,
+    env: 'SALT',
+    default: null,
+  },
+  DB_HOST: {
+    doc: 'Database host (IP or URL)',
+    format: 'url',
+    env: 'DB_HOST',
+    default: '12.0.0.1',
   },
 });
