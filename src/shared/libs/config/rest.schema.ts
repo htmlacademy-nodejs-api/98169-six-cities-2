a89a98b7e212c7447a6ff0e565673c18b4d21d10
Alex <alex.dermenji@space01.co.uk>
2025-07-20 11:34:36 +0100

    add mongoose and update schema

--- src/shared/libs/config/rest.schema.ts
+++ src/shared/libs/config/rest.schema.ts
@@ -7,6 +7,10 @@ export type RestSchema = {
   PORT: number;
   SALT: string;
   DB_HOST: string;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_PORT: string;
+  DB_NAME: string;
 };
 
 export const configRestSchema = convict<RestSchema>({
@@ -24,8 +28,32 @@ export const configRestSchema = convict<RestSchema>({
   },
   DB_HOST: {
     doc: 'Database host (IP or URL)',
-    format: 'url',
+    format: String,
     env: 'DB_HOST',
-    default: '12.0.0.1',
+    default: 'localhost',
+  },
+  DB_USERNAME: {
+    doc: 'Database username',
+    format: String,
+    env: 'DB_USERNAME',
+    default: null,
+  },
+  DB_PASSWORD: {
+    doc: 'Database password',
+    format: String,
+    env: 'DB_PASSWORD',
+    default: null,
+  },
+  DB_PORT: {
+    doc: 'Database port',
+    format: String,
+    env: 'DB_PORT',
+    default: '27017',
+  },
+  DB_NAME: {
+    doc: 'Database name',
+    format: String,
+    env: 'DB_NAME',
+    default: 'six-cities',
   },
 });
